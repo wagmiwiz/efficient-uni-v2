dea55e467db997940b4f50940c8b1e7d4458fd73
wagmiwiz <wagmiwiz@users.noreply.github.com>
2024-08-15 14:04:28 +0100

    swap/addliq/removeliq

---
+++ src/interfaces/IUniswapV2Pair.sol
@@ -0,0 +1,43 @@
+pragma solidity 0.8.24;
+
+interface IUniswapV2Pair {
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint256);
+    function price1CumulativeLast() external view returns (uint256);
+    function kLast() external view returns (uint256);
+
+    function addLiquidity(
+        uint256 amount0Desired,
+        uint256 amount1Desired,
+        uint256 amount0Min,
+        uint256 amount1Min,
+        address to
+    ) external returns (uint256 amount0, uint256 amount1, uint256 liquidity);
+
+    function removeLiquidity(uint256 liquidity, uint256 amount0Min, uint256 amount1Min, address to)
+        external
+        returns (uint256 amount0, uint256 amount1);
+
+    function swapExact(uint256 amountIn, uint256 amountOutMin, bool inIsToken0, address to) external;
+
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+}
